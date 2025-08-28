7fbd92f2357448ee313a5182f113d3decb00f75a
Agus <ariefromjava@gmail.com>
2025-08-29 06:54:48 +0700

    chore(frontend): initial setup with Bun, Vite, Shadcn Vue and TailwindCSS

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="flex items-center justify-center min-h-screen">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+</template>
