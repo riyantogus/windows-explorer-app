6df41b385c62f9fe948ec157dc177e08a3408552
Agus <ariefromjava@gmail.com>
2025-08-28 14:04:00 +0700

    chore: add @elysiajs/cors to enable CORS in backend API

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,13 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import { folderRoutesV1 } from "./routes/folder.route.v1";
 import { fileRoutesV1 } from "./routes/file.route.v1";
 
-const app = new Elysia().use(folderRoutesV1).use(fileRoutesV1).listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(folderRoutesV1)
+  .use(fileRoutesV1)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
